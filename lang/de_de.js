667f6ccb662e50d213243885ed2919f41d04d902
Benjamin Müller <120756894+theinfamousben@users.noreply.github.com>
2025-04-11 08:28:29 +0200

    Update de_de.js

--- lang/de_de.js
+++ lang/de_de.js
@@ -5,7 +5,7 @@ export default {
         "missing": "[[[Fehlende Übersetzung: \"&&ttext&&\"]]]",
         "YouHaveXMoney": "Du hast &&money&& Geld.",
         "WithYourEquation": "Mit deiner Gleichung machst du &&mps&& Geld pro Sekunde.",
-        "productionLineWarning.TooManyUsesOfX": "Die Operation &&cusVar&& wird zu oft benutzt!",
+        "productionLineWarning.TooManyUsesOfX": "&&cusVar&& wird zu oft benutzt!",
         "productionLineWarning.TooManyUsesOfNumberX": "Die Zahl &&cusVar&& kannst du nur einmal benutzen!",
         "productionLineWarning.XNotUnlocked": "&&cusVar&& wurde noch nicht entsperrt!",
         "productionLineWarning.HigherThanCap": "Deine Gleichung macht mehr Geld als deine Produktionsobergrenze! (&&mpsCap&&)",
